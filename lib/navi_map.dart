7a8e7c3f986ff63705e8b7e75f2fd9b46287b74a
ccHarvestasya <harvestasya84@gmail.com>
2023-06-03 20:20:36 +0900

    エルピス地図表示

--- lib/navi_map.dart
+++ lib/navi_map.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:loggy/loggy.dart';
 
 class NaviMap extends StatefulWidget {
   const NaviMap({super.key, required this.title});
@@ -17,14 +20,37 @@ class _NaviMapState extends State<NaviMap> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('NaviMapですよ'),
-            ],
+      body: SafeArea(
+        child: FlutterMap(
+          options: MapOptions(
+            crs: CrsSimple(),
+            center: LatLng(-0.5, 0.5),
+            zoom: 1.0,
+            maxZoom: 3.0,
           ),
+          nonRotatedChildren: const [
+            SimpleAttributionWidget(
+              source:
+                  Text('2010-2023 SQUARE ENIX CO., LTD.\nAll Rights Reserved.'),
+            ),
+          ],
+          children: [
+            TileLayer(
+              tileProvider: AssetTileProvider(),
+              urlTemplate: "assets/images/elpis/{z}/{x}/{y}.jpg",
+              backgroundColor: Theme.of(context).colorScheme.background,
+            ),
+            MarkerLayer(
+              markers: [
+                Marker(
+                  width: 80.0,
+                  height: 80.0,
+                  point: LatLng(0, 0),
+                  builder: (ctx) => const FlutterLogo(),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
